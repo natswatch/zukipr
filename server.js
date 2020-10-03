038cb58714539a8809eb2b46c9b77182e267e733
natswatch <nathaliec@me.com>
2020-10-03 16:19:44 -0700

    queries and req.params

--- server.js
+++ server.js
@@ -41,6 +41,15 @@ function filterByQuery(query, animalsArray) {
     }
     return filteredResults;
 };
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    if (result) {
+    return result;
+    }
+    else {
+        res.send(404);
+    }
+}
 app.get('/api/animals', (req,res) =>{
     let results = animals;
     if (req.query) {
@@ -48,6 +57,10 @@ app.get('/api/animals', (req,res) =>{
     }
     res.json(results);
 });
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    res.json(result);
+})
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
