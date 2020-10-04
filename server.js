ae8fe399f75d56758b1c14fa2457600f62f407b1
natswatch <nathaliec@me.com>
2020-10-03 18:43:01 -0700

    frontend

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true}));
 //parse incoming JSON data
 app.use(express.json());
+app.use(express.static('public'));
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray =[];
@@ -96,10 +97,12 @@ app.get('/api/animals', (req,res) =>{
     }
     res.json(results);
 });
+
 app.get('/api/animals/:id', (req, res) => {
     const result = findById(req.params.id, animals);
     res.json(result);
 });
+
 app.post('/api/animals', (req, res) => {
     //set id based on what the next index of the array will be
     req.body.id = animals.length.toString();
@@ -113,6 +116,10 @@ app.post('/api/animals', (req, res) => {
     const animal = createNewAnimal(req.body, animals);
     res.json(animal);
     }
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
